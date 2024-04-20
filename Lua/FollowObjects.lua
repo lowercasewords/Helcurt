75f64bc003b53ee8df35fdacf7e1b2c6d7f0af94
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2024-04-19 22:36:16 -0700

    feature: follow item is corectly placed behind the character

--- Lua/FollowObjects.lua
+++ Lua/FollowObjects.lua
@@ -1,33 +1,14 @@
-
-
-local STAND_FRAME_TICS = 5
-
+--[[
 addHook("FollowMobj", function(player, mo)
+
+    -- CorrectRotationHoriz(mo, player.mo.x, player.mo.y,
+	-- 								player.mo.x-player.mo.radius, 
+	-- 								player.mo.y, 
+	-- 								player.mo.z+player.mo.height*2, mo.angle)
+
 
     -- if(not Valid(player, "helcurt") or not PAlive(player) or not Valid(mo)) then
     --     return nil
     -- end
-
-    if(mo.frame_timer == nil) then
-        mo.frame_timer = 0
-    end
-
-    --Temporary solution to setting
-    mo.spritexscale = skins[player.skin].highresscale
-    mo.spriteyscale = skins[player.skin].highresscale
-    
-    mo.scale = player.mo.scale
-    
-    
-    if(mo.frame_timer <= 0) then
-        if(mo.frame < C) then
-            mo.frame = $+1
-        else 
-            mo.frame = 0
-        end
-        mo.frame_timer = STAND_FRAME_TICS
-    else
-        mo.frame_timer = $-1
-    end
-
 end, MT_FOLLOW)
+]]--
