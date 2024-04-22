a7b50784b1bd2926eb5db025285b05854753d2c8
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2024-04-22 00:03:39 -0700

    feature-fix: sector highlight and fps improvements

--- Lua/Particles.lua
+++ Lua/Particles.lua
@@ -34,37 +34,41 @@ addHook("PlayerThink", function(player)
 		counter = MAX_TICS
 		searchBlockmap("lines", function(playmo, line)
 			
+			--Checks if the area is dark, retrieves either a fof or sector
 			local area = GetDarkArea(line.frontsector, CONCEAL_DARKNESS_LEVEL, line.frontsector.floorheight)
-			print("  X : "..player.mo.x/FRACUNIT)
-			print("  Y : "..player.mo.y/FRACUNIT)
 
 			--If dark area (either sector or fof was found)
 			if(area ~= nil) then
+				local linesarr = nil
+				local bottom = nil
+				local top = nil
+
 				if(area.bottomheight ~= nil) then --If a FOF (floor over floor)
+					linesarr = area.target.lines
+					bottom = area.target.floorheight
+					top = area.bottomheight
+				else -- if a sector
+					linesarr = area.lines
+					bottom = area.floorheight
+					top = area.ceilingheight
+				end
 
-					local linesarr = area.target.lines
-					
-					if(#linesarr >= 3) then
-						local l1 = linesarr[P_RandomRange(0, #linesarr-1)]
+					--Spawning behavior (chooses between current and random line of the sector)
+					if(linesarr ~= nil and #linesarr >= 3) then
+
+						local l1 = line --CAN'T THE FIRST LINE BE THE LINE IN THE BLOCKMAP FUNCTINO???
 						local l2 = linesarr[P_RandomRange(0, #linesarr-1)]
-
-						-- print("1 x1: "..l1.v1.x/FRACUNIT)
-						-- print("1 y1: "..l1.v1.y/FRACUNIT)
 
 						local x = P_RandomRange(l1.v1.x/FRACUNIT, l2.v1.x/FRACUNIT)*FRACUNIT
 						local y = P_RandomRange(l1.v1.y/FRACUNIT, l2.v1.y/FRACUNIT)*FRACUNIT
 
-						P_SpawnMobj(x, y, P_RandomRange(area.target.floorheight/FRACUNIT, area.bottomheight/FRACUNIT)*FRACUNIT, MT_SHDW).state = S_SHDW_HINT
-						P_SpawnMobj(x, y, P_RandomRange(area.target.floorheight/FRACUNIT, area.bottomheight/FRACUNIT)*FRACUNIT, MT_SHDW).state = S_SHDW_HINT
+						--Spawn with immediate state change (look in the end of the line I hope it's not changed in the end bc I would look dumb)
+						P_SpawnMobj(x, y, P_RandomRange(bottom/FRACUNIT, top/FRACUNIT)*FRACUNIT, MT_SHDW).state = S_SHDW_HINT
+						P_SpawnMobj(x, y, P_RandomRange(bottom/FRACUNIT, top/FRACUNIT)*FRACUNIT, MT_SHDW).state = S_SHDW_HINT
 						
 					end
 
 					return false
-				else -- if a sector
-					print("sector")
-					-- P_SpawnMobj(playmo.x, playmo.y, area.floorheight + 15*FRACUNIT, MT_STGP)
-				end
-
 				
 			end
 			end, 
